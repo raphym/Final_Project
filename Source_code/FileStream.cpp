9fb048c985b0a53ee7e5a29edc05e5e3122323ab
Raphael <elieraphm@gmail.com>
2016-11-15 18:35:06 +0200

    fix problems in fileStream,added functionality

--- Source_code/FileStream.cpp
+++ Source_code/FileStream.cpp
@@ -38,51 +38,67 @@ int FileStream::LineCounters()
 //function which fill the differents arrays (names, positions of x , positions of y )
 int FileStream::fill_ObjectsInfo_Into_Arrays(int nbLines,string *names, double *posX, double *posY,string inputFile)
 {
-        string str2;
-        int i=0;
-        int j=1;
-        double k=0;
+
+        string str;
+        int i=0,line=0;
+        double check=0;
         ifstream file(name.c_str()); //open the file
         if ( !file )
         {
                 cout << "ERROR TO OPEN THE FILE" << endl;
-                exit(-1);
+                return 0;
         }
-
-        for( i=0; i<nbLines; i++,j++)
+        else
         {
-                file >> names[i]; //name of the object
-                try //for the locationX ; its check if it is a number
+                while(std::getline(file, str)) //each time read single line until there is not
                 {
-                        file >> str2;
-                        k=stod(str2);
+                        line++; // other line
+                        vector <string> v; // a string vector
+                        split(str, ' ', v); //split the line into vector v
+                        if(v.size()!=3) //if there is not 3 arguments so errors
+                        {
+                                nbLines--;
+                                cerr <<endl<< "Invalid argument in file : "<<inputFile <<" line : "  << line << '\n';
+                                continue;
+                        }
+                        else //there is exactely 3 arguments so check them
+                        {
+                                try //for the locationX and locationY; its check if its is a number
+                                {
+                                        names[i] = v[0];
+
+                                        check=stod(v[1]); //check the locationX
+                                        posX[i]=check;
+
+
+                                        check=stod(v[2]); //check the locationY
+                                        posY[i]=check;
+
+                                        i++; //increment the index for the arrays names , posX ,posY
+                                }
+                                catch (const std::invalid_argument& ia) //if not a number so the line is not valide go to the next line
+                                {
+                                        cerr <<endl<< "Invalid argument in filed : "<<inputFile <<" line : "  << line << '\n';
+                                        nbLines--;
+                                }
+                        }
                 }
-                catch (const std::invalid_argument& ia) //if not a number so the line is not valide go to the next line
-                {
-                        std::cerr <<endl<< "Invalid argument in file : "<<inputFile <<" line : "  << j << '\n';
-                        i--;
-                        nbLines--;
-                        std::getline(file, str2);
-                        continue;
-                }
-                posX[i]=k;
-                try //for the locationY ; its check if it is a number
-                {
-                        file >> str2;
-                        k=stod(str2);
-                }
-                catch (const std::invalid_argument& ia) //if not a number so the line is not valide go to the next line
-                {
-                        std::cerr <<endl<< "Invalid argument in file : "<<inputFile <<" line : "  << j << '\n';
-                        i--;
-                        nbLines--;
-                        std::getline(file, str2);
-                        continue;
-                }
-                posY[i]=k;
-                std::getline(file, str2);
         }
         file.close();
+        return nbLines;
+}
+//function which split a string into a vector
+void FileStream::split(const string& s, char delim,vector<string>& v)
+{
+        auto i = 0;
+        auto pos = s.find(delim);
+        while (pos != string::npos)
+        {
+                v.push_back(s.substr(i, pos-i));
+                i = ++pos;
+                pos = s.find(delim, pos);
 
-        return nbLines; //return the num of the lines to know if there was invalid lines
+                if (pos == string::npos)
+                        v.push_back(s.substr(i, s.length()));
+        }
 }
--- Source_code/FileStream.h
+++ Source_code/FileStream.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
 
 
 class FileStream
@@ -12,6 +13,8 @@ public:
         FileStream(std::string n);
         virtual ~FileStream();
         int option;
+        //function which split a string into a vector
+        void split(const std::string& s, char delim,std::vector<std::string>& v);
         //return the number of lines which are in the file
         int LineCounters();
         //function which fill the differents arrays (names, positions of x , positions of y )
