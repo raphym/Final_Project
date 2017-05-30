d20b7c4c256dfdd6a4641e447fbf9970c762406e
Raphael <elieraphm@gmail.com>
2017-05-30 17:10:06 +0300

    beautify the code fileStream

--- source_code/FileStream.h
+++ source_code/FileStream.h
@@ -10,20 +10,17 @@
 class FileStream
 {
 public:
-        FileStream(std::string n);
-        virtual ~FileStream();
-        int option;
-        //function which split a string into a vector
-        void split(const std::string& s, char delim,std::vector<std::string>& v);
-        //return the number of lines which are in the file
-        int LineCounters();
-        //function which fill the differents arrays (names, positions of x , positions of y )
-        int fill_ObjectsInfo_Into_Arrays(int nbLines, std::string *names,double *posX, double *posY,std::string inputFile);
+FileStream(std::string n); //constructor
+virtual ~FileStream(); //destructor
+int option; //option
+void split(const std::string& s, char delim,std::vector<std::string>& v); //function which split a string into a vector
+int LineCounters(); //return the number of lines which are in the file
+int fill_ObjectsInfo_Into_Arrays(int nbLines, std::string *names,double *posX, double *posY,std::string inputFile); //function which fill the differents arrays (names, positions of x , positions of y )
 
 
 protected:
 
 private:
-        std::string name;
+std::string name; //name of the file
 };
 #endif // FILESTREAM_H
